0268a2a60697b0aa575ee453697d8c7a6898a8f2
prsanjay <lnvg530@gmail.com>
2015-08-16 08:57:56 +0530

    Initialize repo with complete functionality

---
+++ app/mailers/contact_me.rb
@@ -0,0 +1,9 @@
+class ContactMe < ApplicationMailer
+
+	def mail_from_user(user)
+    @user = user
+    mail(from: @user.email)
+    mail(to: 'lnvg530@gmail.com', subject: 'Mail from portfolio')
+  end
+
+end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :comment, :created_at, :updated_at
