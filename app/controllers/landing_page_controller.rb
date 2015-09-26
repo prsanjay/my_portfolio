0700a740823a6698a92f80d5efce09c9ad68bd5a
prsanjay <lnvg530@gmail.com>
2015-09-26 20:56:37 +0530

    Make experiance count dynamic

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,13 @@
 class LandingPageController < ApplicationController
   def show
+    # calculate experience months or years
+    @total_calculated_exp = ((Time.now.to_date - '2015-04-01'.to_date).to_f)/30.to_f
+
+     @total_experiance = @total_calculated_exp > 12 ? @total_calculated_exp/12.to_f : (@total_calculated_exp)
+
+     @experiance_fraction = @total_experiance - @total_experiance.to_i
+
+     @round_exp = @experiance_fraction > 0.09 ? (@total_experiance).round(1) : (@total_experiance).to_i
+
   end
 end
