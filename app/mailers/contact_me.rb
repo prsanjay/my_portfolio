6e15b4283e3fe6c71eb82e8e891286e1c3087336
prsanjay <lnvg530@gmail.com>
2015-12-26 19:54:06 +0530

    Add delayed job for email queing

--- app/mailers/contact_me.rb
+++ app/mailers/contact_me.rb
@@ -1,9 +1,9 @@
 class ContactMe < ApplicationMailer
 
-	def mail_from_user(name,email,comment)
-    @name = name
-    @email = email
-    @comment = comment
+	def mail_from_user(user)
+    @name = user.name
+    @email = user.email
+    @comment = user.comment
     mail(from: @email)
     mail(to: 'lnvg530@gmail.com', subject: 'Mail from portfolio')
   end
